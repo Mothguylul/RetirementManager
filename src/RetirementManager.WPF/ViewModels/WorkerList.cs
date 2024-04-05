a04a6a6b51db4753bd6d172ed5b889d284e013ea
mothguylul <juliusmuench89gmail.com>
2024-04-05 21:39:59 +0200

    First displays of Workers, First XAML

---
+++ src/RetirementManager.WPF/ViewModels/WorkerList.cs
@@ -0,0 +1,25 @@
+﻿using RetirementManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetirementManager.WPF.ViewModels
+{
+    public class WorkerList : ViewModelBase
+    {
+        public ObservableCollection<Worker> Workers { get; private set; }
+
+        public WorkerList()
+        {
+            Workers = new ObservableCollection<Worker>();
+
+            foreach (Worker worker in DataAccess.Repository.GetWorkers())
+            {
+                Workers.Add(worker);
+            }
+        }
+    }
+}
